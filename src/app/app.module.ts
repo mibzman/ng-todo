f173fbe67b62240bf779c4ee1af5c5855afcd42d
mibzman <sam@hungerperks.com>
2018-08-09 10:33:43 -0400

    form works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { ToDoComponent } from './to-do/to-do.component';
-import { ToDoItemComponent } from './to-do/to-do-item/to-do-item.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { ToDoComponent } from "./to-do/to-do.component";
+import { ToDoItemComponent } from "./to-do/to-do-item/to-do-item.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ToDoComponent,
-    ToDoItemComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ToDoComponent, ToDoItemComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -11,10 +11,18 @@ export class ToDoComponent implements OnInit {
 
 	ToDos: ToDo[] = [new ToDo("clean the house"), new ToDo("buy milk")];
 
+	Input: string;
+
 	ngOnInit() {}
 
 	deleteItem(i: number) {
 		//note delete doesn't work: https://stackoverflow.com/a/40462431/2653503
 		this.ToDos.splice(i, 1);
+	}
+
+	addItem() {
+		console.log(this.ToDos);
+		this.ToDos = this.ToDos.concat(new ToDo(this.Input));
+		console.log(this.ToDos);
 	}
 }
